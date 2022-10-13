efe88b075f3d112f14335b8b9adcf51274c59287
polikar4 <96091712+polikar4@users.noreply.github.com>
2022-10-13 06:22:29 +0300

    push

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿using System.Windows.Forms;
+
+namespace GameWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        
+        private void CreateTable()
+        {
+            int x = 5, y = 4;
+            // create tabel
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            
+            // create labels 
+            Label[,] labels = new Label[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    labels[i, j] = new Label();
+                    labels[i, j].Text = (i * j).ToString();
+                    labels[i, j].Name = "label" + i.ToString() + j.ToString();
+                    labels[i, j].Location = new System.Drawing.Point(0, 0);
+                    labels[i, j].TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                }
+            }
+            // params 
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.ColumnCount = x;
+            this.tableLayoutPanel1.RowCount = y;
+            this.tableLayoutPanel1.Name = "GameTable";
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(this.ClientSize.Width, 300);
+            this.tableLayoutPanel1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            // add column and row
+            for (int i = 0; i < x; i++)
+            {
+                this.tableLayoutPanel1.ColumnStyles.Add(
+                    new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100f/x));
+            }
+            for(int i = 0; i < y; i++)
+            {
+                this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100f/y));
+            }
+            // add label
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    this.tableLayoutPanel1.Controls.Add(labels[i, j], i, j);
+                }
+            }
+            // start logic 
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.Controls.Add(this.tableLayoutPanel1);
+        }
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(663, 600);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            CreateTable();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GameLogic.Construkt(tableLayoutPanel1);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            GameLogic.KeyUp(e.KeyValue);
+        }
+    }
+
+    public static class GameLogic //singletone? hvhvhvhvh. NO
+    {
+        static int x = 5, y = 4; // size table
+        static int now_x = 1, now_y = 1; // now posision 
+        static TableLayoutPanel table; 
+        public static void Construkt(TableLayoutPanel _table)
+        {
+            table = _table;
+        }
+
+        public static void KeyUp(int key)
+        {
+            if(now_x <= x)
+            {
+                Control c = table.Controls.Find("label" + (now_x-1).ToString() + (now_y-1).ToString(), true)[0];
+                c.Text = ((char)key).ToString();
+                now_x++;
+            }
+
+            if(now_x > x && key == 32 && now_y < y)
+            {
+                if(TruWord(x))
+                now_y++;
+                now_x = 1;
+            }
+        }
+
+
+        private static bool TruWord(int len)
+        {
+            // Checking the existence of the word
+            // Maybe in our dictionary, maybe via the Internet
+
+            return true;
+        }
+    }
+}
